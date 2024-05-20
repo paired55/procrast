cd44d929d2856a1093f6ba118b75952c73987c54
Salah Ahmed <riddenariel@gmail.com>
2024-05-20 19:21:21 +0300

    Add login page

--- src/app/shared/layouts/master/master.component.ts
+++ src/app/shared/layouts/master/master.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-master',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, HeaderComponent],
   templateUrl: './master.component.html',
   styleUrl: './master.component.scss',
 })
